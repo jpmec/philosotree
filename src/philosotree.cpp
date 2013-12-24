2e1d38ed34c99e948164ad731dc9e37478c87491
Joshua Petitt <joshpetitt@jpmec.com>
2013-12-23 21:31:54 -0600

    Added Makefile and main()

---
+++ src/philosotree.cpp
@@ -0,0 +1,20 @@
+
+#include <iostream>
+
+
+using namespace std;
+
+
+
+
+int main(int argc, char* argv[])
+{
+  cout << __FILE__ << endl;
+
+  for (int i = 0; i < argc; ++i)
+  {
+    cout << argv[i] << endl;
+  }
+
+  return 0;
+}
