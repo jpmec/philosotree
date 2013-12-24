b180c93c9ded16369c55686028d85c76b8c2dcc6
Joshua Petitt <joshpetitt@jpmec.com>
2013-12-23 22:16:10 -0600

    Updated Makefile and philosotree.cpp. Now can build with #include <boost/asio.hpp>

--- src/philosotree.cpp
+++ src/philosotree.cpp
@@ -1,8 +1,14 @@
 
-#include <iostream>
 
+#include <iostream>
+#include <map>
+#include <set>
+#include <string>
+
+#include <boost/asio.hpp>
 
 using namespace std;
+
 
 
 
@@ -11,10 +17,20 @@ int main(int argc, char* argv[])
 {
   cout << __FILE__ << endl;
 
+  // key is url, value is set of urls contained by key url
+  map< string, set<string> > forward_links;
+
+  // key is url, value is set of urls containing key url
+  map< string, set<string> > reverse_links;
+
+
+
   for (int i = 0; i < argc; ++i)
   {
     cout << argv[i] << endl;
   }
+
+
 
   return 0;
 }
